c202c5d054a9f389f902ed094d814448602ba92f
stefy-Godlove <stefygodlove@gmail.com>
2024-05-03 20:55:36 +0200

    second commit

---
+++ projet tutore/html/body.php
@@ -0,0 +1,26 @@
+<head>
+    <link rel="stylesheet" href="../fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="../css/body.css">
+</head>
+
+
+<body>
+    <!--first part of the body-->
+    <div class="head-body">
+        <!--chevron-left-->
+        <div class="chevron-left">
+            <button id="chevron-left">
+                <i class="fa-solid fa-chevron-left"></i>
+            </button>
+        </div>
+        <!--texte descriptif des images-->
+        <div class="texte">
+            <h2>APPAREIL ELECTROMENAGER CUISINE</h2>
+        </div>
+        <div class="image-card">
+            <div class="image">
+                <img src="../assets/IMG-20240424-WA0007.jpg" alt="">
+            </div>
+        </div>
+    </div>
+</body>
